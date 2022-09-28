8378fde6f3ccbe2516567694b2f41ff385fa9e9e
darek vassalli <98399869+DarekV@users.noreply.github.com>
2022-09-28 11:50:57 +0200

    fin js et position

--- dist/index.18dbc454.js
+++ dist/index.18dbc454.js
@@ -532,6 +532,20 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"1SICI":[function(require,module,exports) {
+var button = document.querySelector(".button");
+var on = function() {
+    button.classList.toggle("on");
+};
+button.addEventListener("click", on);
+var black = document.querySelector(".black");
+var invisible1 = document.querySelector(".invisible-1");
+var invisible2 = document.querySelector(".invisible-2");
+var show = function() {
+    invisible1.classList.add("show");
+    invisible2.classList.add("show");
+    black.classList.add("invisible-1");
+};
+black.addEventListener("click", show);
 
 },{}]},["jQVXF","1SICI"], "1SICI", "parcelRequire0300")
 
